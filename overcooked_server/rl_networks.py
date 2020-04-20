c3f256139bfae4507c56a3aaf7b7a42707b424ad
Xi Rong Sidney Tio <t0916980@kelaberetiv.sg>
2020-04-21 02:21:51 +0800

    Almost completed transfer to RL_trainer; pending a few QoL code updates. Yet to be tested

---
+++ overcooked_server/rl_networks.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvMLPNetwork(nn.Module):
+    """"CNN and MLP network to process OvercookedAI world_state. 
+    Adapted from https://github.com/HumanCompatibleAI/human_aware_rl/blob/master/human_aware_rl/baselines_utils.py"""
+    
+    def __init__(self, input_size, output_size, params):
+        super(ConvMLPNetwork, self).__init__()
+        num_hidden_layers = params["NUM_HIDDEN_LAYERS"]
+        size_hidden_layers = params["SIZE_HIDDEN_LAYERS"]
+        num_filters = params["NUM_FILTERS"]
+        num_convs = params["NUM_CONV_LAYERS"]
+        shape = params['OBS_STATE_SHAPE']
+
+        #Need to double check the padding and calculations
+        self.conv_initial = nn.Conv2d(input_size, num_filters, 5, padding = self.calculate_padding(5, 'same'))
+        self.conv_layers = nn.ModuleList([nn.Conv2d(num_filters, num_filters, kernel_size = 3, padding = self.calculate_padding(5, 'same')) for i in range(0,num_convs - 2)])
+        self.conv_final = nn.Conv2d(num_filters, num_filters, kernel_size = 3, padding = self.calculate_padding(3, 'valid'))
+        self.linear_initial = nn.Linear(shape[0] * shape[1] * num_filters, size_hidden_layers)
+        self.linear_layers = nn.ModuleList([nn.Linear(size_hidden_layers, size_hidden_layers) for i in range(num_hidden_layers)])
+        self.linear_final = nn.Linear(size_hidden_layers, output_size)
+
+    def forward(self, x):
+        x = self.conv_initial(x)
+        for convs in self.conv_layers:
+            x = F.leaky_relu(convs(x))
+            print (x.shape)
+        x = self.conv_final(x)
+        print (x.shape)
+        x = x.view(x.size()[0], -1)
+        x = self.linear_initial(x)
+        for linear in self.linear_layers:
+            x = F.leaky_relu(linear(x))
+        x = F.softmax(self.linear_final(x))
+        return x
+        
+    def calculate_padding(self, kernel, pad_type):
+        if pad_type == 'valid':
+            return 0
+        if pad_type == 'same':
+            return int((kernel - 1) / 2)
--- overcooked_server/rl_trainer.py
+++ overcooked_server/rl_trainer.py
@@ -1,88 +1,172 @@
 import copy
-
+import random
+import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import optim
 
+from .rl_networks import ConvMLPNetwork
+from .utilities.rl_utils import flip_array, vectorize_world_state
 
 class PPOTrainer():
+    """Master class to orchestrate training of PPO Algorithm in Overcooked AI"""
     def __init__(self, config):
         self.config = config
-
+        self.logger = self.setup_logger()
         self.policy_new = self.create_NN(input_dim, output_dim, hyperparameters) # what are the configs for inputdim, outputdim, and hyperparams?
         self.policy_old = self.create_NN(input_dim, output_dim, hyperparameters)
         self.policy_old.load_state_dict(copy.deepcopy(self.policy_new.state_dict()))
         self.policy_new_optim = optim.Adam(self.policy_new.parameters(), lr = self.config['learning_rate'], eps=1e-4)
         self.episode_number = 0
+        self.timesteps_number = 0
         self.many_episode_states = []
         self.many_episode_actions = []
         self.many_episode_rewards = []
-
         self.setup_agents()
+        self.reset_game()
 
     def setup_agents(self):
+        # Parse through configs and init agents, append it back to the env. 
         pass
     
+    def reset_game(self):
+        # how to handle this for multi-agent?
+        self.current_episode_state = []
+        self.current_episode_action = []
+        self.current_episode_rewards= []
+
+    def update_learning_rate(self, starting_lr, optimizer):
+        pass
+
+    def pick_action(self, state, exploration_episilon):
+        if random.random() <= exploration_episilon:
+            action = random.randint(0, self.output_dim - 1)
+            return action
+        
+        state = torch.from_numpy(state).float()
+        actor_output = self.policy_new.forward(state)
+        if self.action_choice_output_columns is not None: #whats this?
+            actor_output = actor_output[:, self.action_choice_output_columns]
+        action_distribution = create_actor_distribution(self.action_types, actor_output, self.action_size)
+        action = action_distribution.sample().cpu()
+        return action
+
     def step(self, world_state):
         action_dict = {}
         #world_state to np array
         #exploration_epsilon =  self.exploration_strategy.get_updated_epsilon_exploration({"episode_number": self.episode_number})
         for agent in agents:
             #flip_array
-            action = agent.pick_action(flipped_arr, exploration_episilon) #maybe just need to do policy.forward dont need to update via agent
+            action = self.pick_action(flipped_arr, exploration_episilon) #maybe just need to do policy.forward dont need to update via agent
             action_dict[agent.agent_id] = [-1, {'goal': [action], 'rewards': -1}]
+        self.timesteps_number += 1
         return action_dict
 
+    def policy_learn(self):
+        all_discounted_returns = self.calculate_all_discounted_returns()
+        if self.hyperparameters["normalise_rewards"]:
+            all_discounted_returns = normalise_rewards(all_discounted_returns)
+        for _ in range(self.hyperparameters["learning_iterations_per_round"]):   #number of epochs 
+            all_ratio_of_policy_probabilities = self.calculate_all_ratio_of_policy_probabilities()
+            loss = self.calculate_loss([all_ratio_of_policy_probabilities], all_discounted_returns)
+            self.take_policy_new_optimisation_step(loss)
 
+    def end_episode(self):
+        self.episode_number += 1
+        self.many_episode_states.append(self.current_episode_state)
+        self.many_episode_actions.append(self.current_episode_action)
+        self.many_episode_rewards.append(self.current_episode_rewards)
+        if episode_number % self.hyperparameters['episodes_per_learning_round'] == 0:
+            loss = self.policy_learn()
+            self.update_learning_rate(self.hyperparameters['learning_rate'], self.policy_new_optimizer)
+            self.equalise_policies()
+        self.reset_game()
+    
+    def calculate_all_ratio_of_policy_probabilities(self):
+        """For each action calculates the ratio of the probability that the new policy would have picked the action vs.
+         the probability the old policy would have picked it. This will then be used to inform the loss"""
+        all_states = [state for states in self.many_episode_states for state in states]
+        all_actions = [[action] if self.action_types == "DISCRETE" else action for actions in self.many_episode_actions for action in actions ]
+        all_states = torch.stack([torch.Tensor(states).float().to(self.device) for states in all_states])
 
+        all_actions = torch.stack([torch.Tensor(actions).float().to(self.device) for actions in all_actions])
+        all_actions = all_actions.view(-1, len(all_states))
 
-        pass
+        new_policy_distribution_log_prob = self.calculate_log_probability_of_actions(self.policy_new, all_states, all_actions)
+        old_policy_distribution_log_prob = self.calculate_log_probability_of_actions(self.policy_old, all_states, all_actions)
+        ratio_of_policy_probabilities = torch.exp(new_policy_distribution_log_prob) / (torch.exp(old_policy_distribution_log_prob) + 1e-8)
+        return ratio_of_policy_probabilities
 
-    def policy_learn():
-        pass
+    def calculate_log_probability_of_actions(self, policy, states, actions):
+        """Calculates the log probability of an action occuring given a policy and starting state"""
+        policy_output = policy.forward(states).to(self.device)
+        policy_distribution = create_actor_distribution("DISCRETE", policy_output, self.action_size)
+        policy_distribution_log_prob = policy_distribution.log_prob(actions)
+        return policy_distribution_log_prob
 
+    def calculate_loss(self, all_ratio_of_policy_probabilities, all_discounted_returns):
+        """Calculates the PPO loss"""
+        all_ratio_of_policy_probabilities = torch.squeeze(torch.stack(all_ratio_of_policy_probabilities))
+        all_ratio_of_policy_probabilities = torch.clamp(input=all_ratio_of_policy_probabilities,
+                                                        min = -sys.maxsize,
+                                                        max = sys.maxsize)
+        all_discounted_returns = torch.tensor(all_discounted_returns).to(all_ratio_of_policy_probabilities)
+        potential_loss_value_1 = all_discounted_returns * all_ratio_of_policy_probabilities
+        potential_loss_value_2 = all_discounted_returns * self.clamp_probability_ratio(all_ratio_of_policy_probabilities)
+        loss = torch.min(potential_loss_value_1, potential_loss_value_2)
+        loss = -torch.mean(loss)
+        return loss
 
+    def take_policy_new_optimisation_step(self, loss):
+        """Takes an optimisation step for the new policy"""
+        self.policy_new_optimizer.zero_grad()  # reset gradients to 0
+        loss.backward()  # this calculates the gradients
+        torch.nn.utils.clip_grad_norm_(self.policy_new.parameters(), self.hyperparameters[
+            "gradient_clipping_norm"])  # clip gradients to help stabilise training
+        self.policy_new_optimizer.step()  # this applies the gradients
+
+    def clamp_probability_ratio(self, value):
+        """Clamps a value between a certain range determined by hyperparameter clip epsilon"""
+        return torch.clamp(input=value, min=1.0 - self.hyperparameters["clip_epsilon"],
+                                  max=1.0 + self.hyperparameters["clip_epsilon"])
+
+    def equalise_policies(self):
+        """Sets the old policy's parameters equal to the new policy's parameters"""
+        for old_param, new_param in zip(self.policy_old.parameters(), self.policy_new.parameters()):
+            old_param.data.copy_(new_param.data)
+    
     def create_NN(self, input_dim, output_dim, hyperparameters):
         return ConvMLPNetwork(input_dim, output_dim, hyperparameters)
 
+    def setup_logger(self):
+        """Sets up the logger"""
+        filename = "Training.log"
+        try: 
+            if os.path.isfile(filename): 
+                os.remove(filename)
+        except: pass
 
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+        # create a file handler
+        handler = logging.FileHandler(filename)
+        handler.setLevel(logging.INFO)
+        # create a logging format
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        # add the handlers to the logger
+        logger.addHandler(handler)
+        return logger
 
-class ConvMLPNetwork(nn.Module):
-    """"CNN and MLP network to process OvercookedAI world_state. 
-    Adapted from https://github.com/HumanCompatibleAI/human_aware_rl/blob/master/human_aware_rl/baselines_utils.py"""
-    
-    def __init__(self, input_size, output_size, params):
-        super(ConvMLPNetwork, self).__init__()
-        num_hidden_layers = params["NUM_HIDDEN_LAYERS"]
-        size_hidden_layers = params["SIZE_HIDDEN_LAYERS"]
-        num_filters = params["NUM_FILTERS"]
-        num_convs = params["NUM_CONV_LAYERS"]
-        shape = params['OBS_STATE_SHAPE']
-
-        #Need to double check the padding and calculations
-        self.conv_initial = nn.Conv2d(input_size, num_filters, 5, padding = self.calculate_padding(5, 'same'))
-        self.conv_layers = nn.ModuleList([nn.Conv2d(num_filters, num_filters, kernel_size = 3, padding = self.calculate_padding(5, 'same')) for i in range(0,num_convs - 2)])
-        self.conv_final = nn.Conv2d(num_filters, num_filters, kernel_size = 3, padding = self.calculate_padding(3, 'valid'))
-        self.linear_initial = nn.Linear(shape[0] * shape[1] * num_filters, size_hidden_layers)
-        self.linear_layers = nn.ModuleList([nn.Linear(size_hidden_layers, size_hidden_layers) for i in range(num_hidden_layers)])
-        self.linear_final = nn.Linear(size_hidden_layers, output_size)
-
-    def forward(self, x):
-        x = self.conv_initial(x)
-        for convs in self.conv_layers:
-            x = F.leaky_relu(convs(x))
-            print (x.shape)
-        x = self.conv_final(x)
-        print (x.shape)
-        x = x.view(x.size()[0], -1)
-        x = self.linear_initial(x)
-        for linear in self.linear_layers:
-            x = F.leaky_relu(linear(x))
-        x = F.softmax(self.linear_final(x))
-        return x
-        
-    def calculate_padding(self, kernel, pad_type):
-        if pad_type == 'valid':
-            return 0
-        if pad_type == 'same':
-            return int((kernel - 1) / 2)
+    def set_random_seeds(self, random_seed):
+        """Sets all possible random seeds so results can be reproduced"""
+        os.environ['PYTHONHASHSEED'] = str(random_seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.manual_seed(random_seed)
+        random.seed(random_seed)
+        np.random.seed(random_seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(random_seed)
+            torch.cuda.manual_seed(random_seed)
