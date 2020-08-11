bf79c206afdeef9df3cb5469696acbf5a7e8df4d
sidney_tio <sidney_tio@aisingapore.rog>
2020-08-12 01:24:29 +0800

    Testing video functionality

--- overcooked_server/game.py
+++ overcooked_server/game.py
@@ -18,16 +18,17 @@ from settings import *
 
 import helpers
 
+
 class Game:
     def __init__(
         self,
-        num_ai_agents: int=1,
-        num_rl_agents: int=0,
-        RLTrainer= None,
-        is_simulation: bool=False,
-        simulation_episodes: int=500,
-        is_tom: bool=False,
-        experiment_id: str='1'
+        num_ai_agents: int = 1,
+        num_rl_agents: int = 0,
+        RLTrainer=None,
+        is_simulation: bool = False,
+        simulation_episodes: int = 500,
+        is_tom: bool = False,
+        experiment_id: str = '1'
     ) -> None:
         pg.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -46,7 +47,7 @@ class Game:
             idx_str = str(idx)
             AI_AGENTS_TO_INITIALIZE[idx_str] = AI_AGENTS[idx_str]
             agent_id = idx
-        
+
         for idx in range(agent_id+1, num_rl_agents+1):
             idx = str(idx)
             RL_AGENTS_TO_INITIALIZE[idx] = AI_AGENTS[idx]
@@ -62,7 +63,7 @@ class Game:
             self.env = OvercookedEnv(
                 ai_agents=AI_AGENTS_TO_INITIALIZE,
                 rl_agents=RL_AGENTS_TO_INITIALIZE,
-                rl_trainer= self.RLTrainer,
+                rl_trainer=self.RLTrainer,
                 queue_episodes=QUEUE_EPISODES
             )
             self.load_data()
@@ -72,13 +73,13 @@ class Game:
             self.results = defaultdict(int)
             self.results_col = []
             for i in range(self.TERMINATING_EPISODE+1):
-                if i%50 == 0:
+                if i % 50 == 0:
                     self.results[str(i)] = 0
                     self.results_col.append(str(i))
 
             self.run_simulation(simulation_episodes)
         else:
-            final_HUMAN_AGENTS = {k:v for k,v in HUMAN_AGENTS.items() if k == '1'}
+            final_HUMAN_AGENTS = {k: v for k, v in HUMAN_AGENTS.items() if k == '1'}
             self.env = OvercookedEnv(
                 human_agents=final_HUMAN_AGENTS,
                 ai_agents=AI_AGENTS_TO_INITIALIZE,
@@ -94,7 +95,7 @@ class Game:
         self.results = defaultdict(int)
         self.results_col = []
         for i in range(self.TERMINATING_EPISODE+1):
-            if i%50 == 0:
+            if i % 50 == 0:
                 self.results[str(i)] = 0
                 self.results_col.append(str(i))
 
@@ -107,7 +108,7 @@ class Game:
         self.PLAYERS, self.PLATES, self.POTS = {}, {}, {}
         self.INGREDIENTS = []
         self.TABLE_TOPS = self._deep_copy(TABLE_TOPS)
-        
+
         RETURN_STATION_OCCUPIED = False
         self.RETURN_STATION = {
             'state': 'empty',
@@ -146,33 +147,34 @@ class Game:
                 ingredient.state,
                 ingredient.location
             ])
-        
+
         if RETURN_STATION_OCCUPIED:
             self.RETURN_STATION = {
                 'state': 'filled',
                 'coords': episode['return_counter']
             }
-        self.CHOPPING_BOARDS = [chopping_board.location for chopping_board in episode['chopping_board'] if chopping_board.state != 'taken']
+        self.CHOPPING_BOARDS = [
+            chopping_board.location for chopping_board in episode['chopping_board'] if chopping_board.state != 'taken']
         self.INGREDIENTS_STATION = INGREDIENTS_STATION
         self.SERVING_STATION = SERVING_STATION
         self.WALLS = WALLS
 
     def new(
         self,
-        players: Dict[int, Tuple[int,int]],
-        table_tops: List[Tuple[int,int]],
+        players: Dict[int, Tuple[int, int]],
+        table_tops: List[Tuple[int, int]],
         ingredients,
-        chopping_boards: List[Tuple[int,int]],
+        chopping_boards: List[Tuple[int, int]],
         plates: Dict[int, List[Any]],
         pots: Dict[int, List[Any]],
-        ingredient_stations: Dict[str, Tuple[int,int]],
-        serving_stations: List[Tuple[int,int]],
-        return_station: List[Tuple[int,int]],
-        walls: List[Tuple[int,int]]=WALLS,
-        score: Tuple[int,int]=SCOREBOARD_SCORE,
-        orders: Tuple[int,int]=SCOREBOARD_ORDERS,
-        timer: Tuple[int,int]=SCOREBOARD_TIMER,
-        scoreboard: List[Tuple[int,int]]=SCOREBOARD
+        ingredient_stations: Dict[str, Tuple[int, int]],
+        serving_stations: List[Tuple[int, int]],
+        return_station: List[Tuple[int, int]],
+        walls: List[Tuple[int, int]] = WALLS,
+        score: Tuple[int, int] = SCOREBOARD_SCORE,
+        orders: Tuple[int, int] = SCOREBOARD_ORDERS,
+        timer: Tuple[int, int] = SCOREBOARD_TIMER,
+        scoreboard: List[Tuple[int, int]] = SCOREBOARD
     ) -> None:
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
@@ -194,20 +196,25 @@ class Game:
         for idx in range(1, self.player_count+1):
             idx = str(idx)
             if idx == '1':
-                self.player_1 = Player(self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
+                self.player_1 = Player(
+                    self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
             elif idx == '2':
-                self.player_2 = Player(self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
+                self.player_2 = Player(
+                    self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
             elif idx == '3':
-                self.player_3 = Player(self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
+                self.player_3 = Player(
+                    self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
             elif idx == '4':
-                self.player_4 = Player(self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
+                self.player_4 = Player(
+                    self, idx, players[idx]['coords'][1], players[idx]['coords'][0], players[idx]['holding'])
         for table_top_coord in table_tops:
             TableTop(self, table_top_coord[1], table_top_coord[0])
         for ingredient in ingredients:
             ingredient_name = ingredient[0]
             ingredient_state = ingredient[1]
             ingredient_coords = ingredient[2]
-            Ingredients(self, ingredient_name, ingredient_state, ingredient_coords[1], ingredient_coords[0])
+            Ingredients(self, ingredient_name, ingredient_state,
+                        ingredient_coords[1], ingredient_coords[0])
         for chopping_board_coord in chopping_boards:
             ChoppingBoardStation(self, chopping_board_coord[1], chopping_board_coord[0])
         for key, val in plates.items():
@@ -225,8 +232,9 @@ class Game:
                 IngredientStation(self, ingredient, coords[1], coords[0])
         for serving_station_coord in serving_stations:
             ServingStation(self, serving_station_coord[1], serving_station_coord[0])
-            
-        ReturnStation(self, return_station['state'], return_station['coords'][1], return_station['coords'][0])
+
+        ReturnStation(self, return_station['state'],
+                      return_station['coords'][1], return_station['coords'][0])
         Score(self, score[1], score[0])
         Orders(self, orders[1], orders[0])
         Timer(self, timer[1], timer[0])
@@ -244,10 +252,12 @@ class Game:
         results_df.to_csv(self.results_filename, index=False)
 
         if not self.is_simulation:
-            self.info_df.to_csv(self.experiment_folder + '/experiments_' + self.env.results_filename + '.csv', index=False)
+            self.info_df.to_csv(self.experiment_folder + '/experiments_' +
+                                self.env.results_filename + '.csv', index=False)
 
             # agent_types = [agent.is_inference_agent for agent in self.env.world_state['agents']]
-            video_name_ext = helpers.get_video_name_ext(self.env.world_state['agents'], self.TERMINATING_EPISODE, MAP)
+            video_name_ext = helpers.get_video_name_ext(
+                self.env.world_state['agents'], self.TERMINATING_EPISODE, MAP)
             # video_name_ext = helpers.get_video_name_ext(agent_types, self.TERMINATING_EPISODE, MAP)
             helpers.make_video_from_image_dir(
                 self.experiment_folder,
@@ -258,9 +268,9 @@ class Game:
     def run(self):
         # game loop - set self.playing = False to end the game
         self.playing = True
-        
+
         while self.playing:
-            if self.env.episode%50 == 0:
+            if self.env.episode % 50 == 0:
                 self.results[str(self.env.episode)] = self.env.world_state['total_score']
             if self.env.episode >= self.TERMINATING_EPISODE:
                 self.save_results()
@@ -308,18 +318,18 @@ class Game:
         )
         orderRect.center = (
             (SCOREBOARD_ORDERS[1]+1)*TILESIZE+TILESIZE//2,
-            HEIGHT-TILESIZE//2    
+            HEIGHT-TILESIZE//2
         )
         countdownRect.center = (
             (SCOREBOARD_TIMER[1]+1)*TILESIZE+TILESIZE//2,
-            HEIGHT-TILESIZE//2    
+            HEIGHT-TILESIZE//2
         )
         self.screen.blit(score, scoreRect)
         self.screen.blit(order, orderRect)
         self.screen.blit(ep_countdown, countdownRect)
         pg.display.flip()
 
-    def events(self):    
+    def events(self):
         # catch all events here
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -337,8 +347,10 @@ class Game:
                     print(f'Current goal info: \n{goal_info}\n')
                     print([agent.holding for agent in self.env.world_state['agents']])
 
-                    player_action_validity, action_type, action_task, goal_id = self.env._check_action_validity(1, event.key)
-                    player_object = [agent for agent in self.env.world_state['agents'] if agent.id == '1'][0]
+                    player_action_validity, action_type, action_task, goal_id = self.env._check_action_validity(
+                        1, event.key)
+                    player_object = [
+                        agent for agent in self.env.world_state['agents'] if agent.id == '1'][0]
                     best_goals = self.env.find_agents_best_goal()
                     print('found best goals')
                     print(best_goals)
@@ -348,11 +360,13 @@ class Game:
                     else:
                         action_mapping, reward_mapping = self._get_action_mapping_info(event.key)
                         if action_type == 'movement':
-                            best_goals[player_object] = [-1, {'steps': [action_mapping], 'rewards': reward_mapping}]
+                            best_goals[player_object] = [-1,
+                                                         {'steps': [action_mapping], 'rewards': reward_mapping}]
                         else:
                             # its a task_action being taken
                             # goal_id = player_object._find_suitable_goal(action_mapping, action_task)
-                            best_goals[player_object] = [goal_id, {'steps': action_task, 'rewards': reward_mapping}]
+                            best_goals[player_object] = [
+                                goal_id, {'steps': action_task, 'rewards': reward_mapping}]
 
                     print('best goals')
                     print(best_goals)
@@ -384,25 +398,25 @@ class Game:
             pg.K_RIGHT: [1, -1],
             pg.K_UP: [2, -1],
             pg.K_DOWN: [3, -1],
-            pg.K_COMMA: [4, -2], # DIAG-UP-LEFT
-            pg.K_PERIOD: [5, -2], # DIAG-UP-RIGHT
-            pg.K_SLASH: [6, -2], # DIAG-DOWN-LEFT
-            pg.K_RSHIFT: [7, -2], # DIAG-DOWN-RIGHT
-            pg.K_m: [8, -2], # STAY
+            pg.K_COMMA: [4, -2],  # DIAG-UP-LEFT
+            pg.K_PERIOD: [5, -2],  # DIAG-UP-RIGHT
+            pg.K_SLASH: [6, -2],  # DIAG-DOWN-LEFT
+            pg.K_RSHIFT: [7, -2],  # DIAG-DOWN-RIGHT
+            pg.K_m: [8, -2],  # STAY
             pg.K_z: [9, 10],
             pg.K_x: [10, 30],
             pg.K_c: [11, 45],
             pg.K_v: [12, 50],
             pg.K_b: [13, 100],
-            pg.K_n: [14, 0], # DROP
+            pg.K_n: [14, 0],  # DROP
         }
         return action_reward_mapping[action][0], action_reward_mapping[action][1]
-    
-
 
     def update_experiment_results(self, info_df):
-        agent_1_info = [(agent.location, agent.last_action) for agent in self.env.world_state['agents'] if agent.id == '1'][0]
-        agent_2_info = [(agent.location, agent.last_action) for agent in self.env.world_state['agents'] if agent.id == '2'][0]
+        agent_1_info = [(agent.location, agent.last_action)
+                        for agent in self.env.world_state['agents'] if agent.id == '1'][0]
+        agent_2_info = [(agent.location, agent.last_action)
+                        for agent in self.env.world_state['agents'] if agent.id == '2'][0]
         temp_info_df = info_df.append({
             'episode': self.env.episode,
             'player_coords': agent_1_info[0],
@@ -429,7 +443,7 @@ class Game:
         # only require historical world_state 1 timestep ago
         temp_copy['historical_world_state'] = {}
         self.env.world_state['historical_world_state'] = temp_copy
-        
+
         reward_mapping = {}
         action_mapping = {}
         for agent in best_goals:
@@ -465,7 +479,7 @@ class Game:
             self.info_df = self.update_experiment_results(self.info_df)
             pg.image.save(self.screen, self.images_folder+f'/episode_{self.env.episode}.png')
 
-    def run_simulation(self, episodes:int=500):
+    def run_simulation(self, episodes: int = 500):
         game_folder = os.path.dirname(__file__)
         simulations_folder = os.path.join(game_folder, 'simulations')
         video_folder = os.path.join(game_folder, 'videos')
@@ -480,7 +494,7 @@ class Game:
         helpers.check_dir_exist(map_folder)
 
         helpers.clean_dir(simulations_folder)
-        
+
         start_time = datetime.now()
         for episode in range(episodes):
             if episode == 0:
@@ -520,12 +534,13 @@ class Game:
             print([agent.holding for agent in self.env.world_state['agents']])
             self.env.update_episode()
             if self.env.rl_trainer:
-                    if self.env.rl_trainer.episode_number % 100 == 0:
-                        pg.image.save(self.screen, simulations_folder+f'/episode_{self.env.episode}.png')
+                if self.env.rl_trainer.episode_number % 100 == 0:
+                    pg.image.save(self.screen, simulations_folder +
+                                  f'/episode_{self.env.episode}.png')
 
             if self.env.episode == 0:
                 self.results[str(self.env.episode)] = self.env.world_state['total_score']
-            if (self.env.episode+1)%50 == 0:
+            if (self.env.episode+1) % 50 == 0:
                 self.results[str(self.env.episode+1)] = self.env.world_state['total_score']
             if self.env.episode == self.TERMINATING_EPISODE:
                 print('saving results')
@@ -544,31 +559,35 @@ class Game:
         end_time = datetime.now()
         experiment_runtime = (end_time - start_time).seconds
         experiment_runtime_min = experiment_runtime//60
-        experiment_runtime_sec = experiment_runtime%60
-        print(f'Simulation Experiment took {experiment_runtime_min} mins, {experiment_runtime_sec} secs to run.')
+        experiment_runtime_sec = experiment_runtime % 60
+        print(
+            f'Simulation Experiment took {experiment_runtime_min} mins, {experiment_runtime_sec} secs to run.')
 
         # agent_types = [agent.is_inference_agent for agent in self.env.world_state['agents']]
         # video_name_ext = helpers.get_video_name_ext(agent_types, episodes, MAP)
         if self.env.rl_trainer:
-            self.env.rl_trainer.log_explicit_results(explicit_chop_rewards, explicit_cook_rewards, explicit_serve_rewards)
-            self.env.rl_trainer.logger.info(f'Simulation Experiment took {experiment_runtime_min} mins, {experiment_runtime_sec} secs to run.')
-            if self.env.rl_trainer.episode_number % 100 == 0:
+            self.env.rl_trainer.log_explicit_results(
+                explicit_chop_rewards, explicit_cook_rewards, explicit_serve_rewards)
+            self.env.rl_trainer.logger.info(
+                f'Simulation Experiment took {experiment_runtime_min} mins, {experiment_runtime_sec} secs to run.')
+            if self.env.rl_trainer.episode_number % 1 == 0:
                 self.env.rl_trainer.logger.info(f'Saving video at {map_folder}')
-                video_name_ext = helpers.get_video_name_ext(self.env.world_state['agents'], 
+                video_name_ext = helpers.get_video_name_ext(self.env.world_state['agents'],
                                                             self.env.rl_trainer.episode_number, MAP)
                 helpers.make_video_from_image_dir(
                     map_folder,
                     simulations_folder,
                     video_name_ext,
                     2
-                    )
-        #sys.exit()
+                )
+        # sys.exit()
 
     def show_start_screen(self):
         pass
 
     def show_go_screen(self):
         pass
+
 
 @click.command()
 @click.option('--num_ai_agents', default=1, help='Number of AI agents to initialize')
@@ -589,14 +608,18 @@ def main(num_ai_agents, num_rl_agents, is_simulation, episodes, simulation_episo
         from rl_config import config
         RLTrainer = PPOTrainer(config)
         RLTrainer.logger.info(str(config.hyperparameters))
-    else: RLTrainer = None
+    else:
+        RLTrainer = None
 
     for episode in range(episodes):
         time_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        RLTrainer.logger.info("======== STARTING EPISODE {} AT {} =======".format(episode+1, time_now))
-        g = Game(num_ai_agents, num_rl_agents, RLTrainer, is_simulation, simulation_episodes, is_tom, experiment_id)
+        RLTrainer.logger.info(
+            "======== STARTING EPISODE {} AT {} =======".format(episode+1, time_now))
+        g = Game(num_ai_agents, num_rl_agents, RLTrainer, is_simulation,
+                 simulation_episodes, is_tom, experiment_id)
     time_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    RLTrainer.logger.info("============= A TOTAL OF {} COMPLETED at {} ==================".format(episodes, time_now))
+    RLTrainer.logger.info(
+        "============= A TOTAL OF {} EPISODES COMPLETED at {} ==================".format(episodes, time_now))
     sys.exit()
     g.show_start_screen()
     while True:
